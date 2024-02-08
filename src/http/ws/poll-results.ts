aa734ff1c0f5c94bbb49e37ffd9d6f1ac0ff9caa
Rafael <rafaelberger.dev@gmailcom>
2024-02-08 13:48:04 -0300

    erro corrigido

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -4,7 +4,7 @@ import { voting } from "../../utils/voting-pub-sub";
 
 export async function pollResults(app: FastifyInstance) {
   app.get(
-    "/polls/:polld/results",
+    "/polls/:pollId/results",
     { websocket: true },
     (connection, request) => {
       const getPollParams = z.object({
